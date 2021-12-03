dace9116bc7973d07df1984a12b1b9f1bd71919b
unknown <tanishdragonemperor>
2021-12-03 22:24:17 +0530

    Updates

---
+++ precache-manifest.b6af981414fa823ee7690130d8d67d91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c01b7691fe6c9d485bf320515366c1d9",
+    "url": "/react-assignment/index.html"
+  },
+  {
+    "revision": "9a21c2ed725226ed32de",
+    "url": "/react-assignment/static/css/main.ca1f696f.chunk.css"
+  },
+  {
+    "revision": "1407c86a95fc80bc2054",
+    "url": "/react-assignment/static/js/2.8ed737be.chunk.js"
+  },
+  {
+    "revision": "9a21c2ed725226ed32de",
+    "url": "/react-assignment/static/js/main.ea7dbb88.chunk.js"
+  },
+  {
+    "revision": "e17c6038473e80b01236",
+    "url": "/react-assignment/static/js/runtime~main.33d0e07e.js"
+  }
+]);
